a1c3dfceba55cbd234f37d1303e5c8c799c53e44
dian-tri-budianto <dian.budianto@berca.co.id>
2021-05-30 09:42:39 +0700

    setup method get all notes and display note by id for using form edit

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 // routes.js : Memuat kode konfigurasi routing server seperti menentukan path, method, dan handler yang digunakan.
 
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 
