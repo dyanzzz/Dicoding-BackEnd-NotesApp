f9ac6b8b01a7a6a0b6d119624897a9a97d0c8b6a
Dian Tri Budianto <dyanzzz@gmail.com>
2022-03-12 22:56:27 +0700

    Register Notes Plugin Using Router and Handler

---
+++ src/api/notes/index.js
@@ -0,0 +1,13 @@
+// notes.js : Memuat data notes yang disimpan dalam bentuk array objek.
+
+const NotesHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler(service);
+    server.route(routes(notesHandler));
+  },
+};
