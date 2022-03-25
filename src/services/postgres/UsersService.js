7079c9c4432feef89b8ee4bef149c3b288e7640d
Dian Tri Budianto <dyanzzz@gmail.com>
2022-03-25 11:13:27 +0700

    Fix type message response error authentication

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -67,7 +67,7 @@ class UsersService {
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
-      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
     }
 
     const { id, password: hashedPassword } = result.rows[0];
